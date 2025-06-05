e4ccaa329ac3acb80d7670b92d9e3886cfb523e6
dheeraj <dheerajkumar12757@gmail.com>
2025-06-06 00:29:53 +0530

    "Dheeraj Hello to C++ !! "
    Replace cpp_udemy.cpp with hello_world.cpp
    
    Removed the cpp_udemy.cpp file and added hello_world.cpp, which prints "Dheeraj, Hello, World!!". Updated project files to reflect this change in the compilation list and source file filters.

---
+++ cpp_udemy/hello_world.cpp
@@ -0,0 +1,11 @@
+// This is a simple C++ program that prints "Hello, World!" to the console.
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Dheeraj, Hello, World!!" << endl;
+	return 0;
+}
+
